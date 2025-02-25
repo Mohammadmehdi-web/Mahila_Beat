1a5f590f2b77c877f7e259469d5d0e47aa2c3f96
Aditya_kumar <webauctech110@gmail.com>
2025-02-25 10:58:59 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Text,
+} from 'react-native';
+
+function App() {
+  
+
+  return (
+    <Text style={{color:"white"}}>hello</Text>
+  );
+}
+
+export default App;
